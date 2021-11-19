7ebb97470400c2a02bdfc7a4a815a99af81b2ff1
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-11-19 16:57:58 +0300

    finished histogram.py

--- histogram.py
+++ histogram.py
@@ -1,15 +1,27 @@
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
-    dataset = np.genfromtxt('./resources/dataset_train.csv', delimiter=',', names=True, dtype=float, filling_values=None)
-    course = ['Arithmancy','Astronomy', 'Herbology',
-     'Defense_Against_the_Dark_Arts', 'Divination', 'Muggle_Studies',
-     'Ancient_Runes', 'History_of_Magic', 'Transfiguration', 'Potions',
-     'Care_of_Magical_Creatures', 'Charms', 'Flying']
-    plt.legend(course)
+    dataset = np.genfromtxt('./resources/dataset_train.csv', delimiter=',',
+                            names=True, filling_values=None, dtype=None,
+                            encoding='UTF-8')
+    course = ['Arithmancy', 'Astronomy', 'Herbology',
+              'Defense_Against_the_Dark_Arts', 'Divination', 'Muggle_Studies',
+              'Ancient_Runes', 'History_of_Magic', 'Transfiguration', 'Potions',
+              'Care_of_Magical_Creatures', 'Charms', 'Flying']
+    legend = {
+        'Gryffindor': 'brown',
+        'Hufflepuff': 'black',
+        'Ravenclaw': 'steelblue',
+        'Slytherin': 'green'}
     for item in course:
         plt.title(item)
-        plt.hist(dataset[:][item], alpha=0.3)
+        for house, color in legend.items():
+            # data = np.extract(dataset[:][0] > 10)
+            data = dataset[dataset['Hogwarts_House'] == house]
+            # data = dataset[numpy.in1d(dataset[:][1], house)]
+            plt.hist(data[:][item], alpha=0.7, label=house, color=color)
+            plt.xlabel('Score')
+            plt.ylabel('Number of students')
+        plt.legend(legend.keys())
         plt.show()
